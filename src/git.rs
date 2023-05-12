22a43fb2fb2881d8193a8e2daf8cc30af9811f68
Jorge Martins <jorge.martins@shattereddev.com>
2023-05-13 00:05:50 +0100

    starting, cloning and deleting working

--- src/git.rs
+++ src/git.rs
@@ -1,120 +1,34 @@
-use anyhow::{Result};
-use git2::{Repository, RemoteCallbacks, build::{CheckoutBuilder, RepoBuilder}, FetchOptions, Progress, Cred};
-use std::{cell::RefCell, path::{PathBuf}, io::{self, Write}, str::FromStr};
-
-struct State {
-    current: usize,
-    path: Option<PathBuf>,
-    progress: Option<Progress<'static>>,
-    total: usize,
-    newline: bool
-}
+use anyhow::{Result, Context};
+use std::fs;
+use std::path::PathBuf;
+use tokio::process::Command;
 
 pub struct RepositoryReference {
-    pub ssh_key: Option<PathBuf>,
     pub path: PathBuf,
-    pub repo: Option<Repository>,
     pub url: String
 }
 
-fn print(state: &mut State) {
-    let stats = state.progress.as_ref().unwrap();
-    let network_pct = (100 * stats.received_objects()) / stats.total_objects();
-    let index_pct = (100 * stats.indexed_objects()) / stats.total_objects();
-    let co_pct = if state.total > 0 {
-        (100 * state.current) / state.total
-    } else {
-        0
-    };
-    let kbytes = stats.received_bytes() / 1024;
-    if stats.received_objects() == stats.total_objects() {
-        if !state.newline {
-            println!();
-            state.newline = true;
-        }
-        print!(
-            "Resolving deltas {}/{}\r",
-            stats.indexed_deltas(),
-            stats.total_deltas()
-        );
-    } else {
-        print!(
-            "net {:3}% ({:4} kb, {:5}/{:5})  /  idx {:3}% ({:5}/{:5})  \
-             /  chk {:3}% ({:4}/{:4}) {}\r",
-            network_pct,
-            kbytes,
-            stats.received_objects(),
-            stats.total_objects(),
-            index_pct,
-            stats.indexed_objects(),
-            stats.total_objects(),
-            co_pct,
-            state.current,
-            state.total,
-            state
-                .path
-                .as_ref()
-                .map(|s| s.to_string_lossy().into_owned())
-                .unwrap_or_default()
-        )
-    }
-    io::stdout().flush().unwrap();
-}
-
 impl RepositoryReference {
-    pub fn clone(&self) -> Result<()> {
-        let state = RefCell::new(State {
-            progress: None,
-            total: 0,
-            current: 0,
-            path: None,
-            newline: false,
-        });
+    pub async fn prepare(&mut self) -> Result<()> {
+        fs::create_dir(&self.path).with_context(|| format!("Failed to create directory {:?}", self.path))?;
 
-        let mut cb = RemoteCallbacks::new();
-        cb.transfer_progress(|stats| {
-            let mut state = state.borrow_mut();
-            state.progress = Some(stats.to_owned());
-            print(&mut *state);
-            true
-        });
-    
-        let mut co = CheckoutBuilder::new();
-        co.progress(|path, cur, total| {
-            let mut state = state.borrow_mut();
-            state.path = path.map(|p| p.to_path_buf());
-            state.current = cur;
-            state.total = total;
-            print(&mut *state);
-        });
+        self.path = self.path.canonicalize().unwrap();
+        println!("Folder created '{:?}'.", self.path);
+        Ok(())
+    }
 
-        if let Some(key_path) = self.ssh_key.clone() {
-            let pvt_key = key_path.canonicalize().unwrap();
-            let pub_key = PathBuf::from_str(&format!("{}.pub", pvt_key.to_str().unwrap())).unwrap();
+    pub async fn clone(&self) -> Result<()> {
+        let clone_result = Command::new("git")
+            .args(["clone", &self.url, "./"])
+            .current_dir(&self.path)
+            .output().await;
 
-            cb.credentials(move |_url, username_from_url, _allowed_types| {
-                println!("->> ssh user {:?}", username_from_url);
-                println!("->> ssh pub key {:?}", pub_key);
-                println!("->> ssh pvt key {:?}", pvt_key);
+        clone_result.with_context(|| format!("Failed to clone repository '{:?}'", &self.url))?;
+        Ok(())
+    }
 
-                Cred::ssh_key(
-                    username_from_url.unwrap(),
-                    Some(&pub_key),
-                    &pvt_key,
-                    None,
-                )
-            });
-        }
-    
-        let mut fo = FetchOptions::new();
-        fo.remote_callbacks(cb);
-        RepoBuilder::new()
-            .fetch_options(fo)
-            .with_checkout(co)
-            .clone(&self.url, &self.path)?;
-        
-        println!();
-    
+    pub async fn cleanup(&self) -> Result<()> {
+        fs::remove_dir_all(&self.path).with_context(|| format!("Failed to delete folder '{:?}", self.path))?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use tokio::{time::Instant, process::Command};
 use uuid::Uuid;
-use std::{fs, io::BufReader, time::Duration, path::Path};
+use std::{fs, io::BufReader, time::Duration, path::{PathBuf}};
 use rust_multi_command::{command::{CommandConfig, TestConfig}, git::RepositoryReference};
 
 #[tokio::main]
@@ -21,45 +21,39 @@ async fn main() -> Result<()> {
                 .map(|test_case| {
                     let mv_command = command.clone();
                     let mv_repository = test_case.repository.clone();
-                    let mv_ssh_key = config.key_file.clone();
                     
                     tokio::spawn(async move {
                         let target_folder = Uuid::new_v4().to_string();
-                        let create_result = fs::create_dir(&target_folder);
+
+                        let mut repo = RepositoryReference { path: PathBuf::from(target_folder), url: mv_repository }; 
+                        let create_result = repo.prepare().await;
 
                         if create_result.is_err() {
-                            println!("Failed to create directory '{:?}'.", target_folder);
-                            return format!("[{:?}] Elapsed {:?}ms on '{:?}' -- '{:?}'.", target_folder, Duration::MAX.as_millis(), mv_command, mv_repository)
+                            println!("->> {:?}", create_result);
+                            return format!("[{:?}] Elapsed {:?}ms on '{:?}' -- '{:?}'.", repo.path, Duration::MAX.as_millis(), mv_command, repo.url)
                         }
 
-                        let target_folder = Path::new(&target_folder).canonicalize().unwrap();
-                        println!("Folder created '{:?}'.", target_folder);
-                        
-                        let repo = RepositoryReference { ssh_key: mv_ssh_key, path: target_folder, url: mv_repository, repo: Option::None };
-                        let clone_result = repo.clone();
+                        let clone_result = repo.clone().await;
 
                         if clone_result.is_err() {
-                            println!("Failed to clone repository '{:?}' -- {:?}", repo.url, clone_result);
-                            let deletion_result = fs::remove_dir_all(&repo.path);
-                            if deletion_result.is_err() {
-                                println!("{:?}", deletion_result);
-                            }   
+                            println!("->> {:?}", clone_result);
+                            let deletion_result = repo.cleanup().await;
+                            if deletion_result.is_err() { println!("->> {:?}", deletion_result); }   
                             return format!("[{:?}] Elapsed {:?}ms on '{:?}' -- '{:?}'.", repo.path, Duration::MAX.as_millis(), mv_command, repo.url)
                         }
 
                         println!("Repository cloned '{:?}' at '{:?}'.", repo.path, repo.url);
                         let start_time = Instant::now();
     
-                        let install_result = Command::new("yarn")
+                        let _install_result = Command::new("yarn")
                             .current_dir(&repo.path)
                             .output().await;
     
-                        println!("{:?}", mv_command);
-                        println!("{:?}", repo.url);
-
-                        // println!("{:?}", install_result.try_into();
-                        
                         // Do stuff
+                        
+                        let deletion_result = repo.cleanup().await;
+                        if deletion_result.is_err() { println!("->> {:?}", deletion_result); }   
+
                         return format!("[{:?}] Elapsed {:?}ms on '{:?}' -- '{:?}'.", repo.path, start_time.elapsed().as_millis(), mv_command, repo.url)
                     })
                 }))
